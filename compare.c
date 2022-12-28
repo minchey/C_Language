bbe8f006e861b3aeed507141f91c4e5d129cbb67
minchey <98521434+minchey@users.noreply.github.com>
2022-12-28 17:16:04 +0900

    비교조건

---
+++ compare.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a<b)
+    {
+        printf("<\n");
+    }
+    else if(a>b)
+    {
+        printf(">\n");
+    }
+    else
+    {
+        printf("==");
+    }
+    return 0;
+}
