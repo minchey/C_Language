9ed5498bad9fef884396d2fbc80da1750b77a978
minchey <98521434+minchey@users.noreply.github.com>
2023-01-12 15:24:25 +0900

    무기강화프로그램

--- weapon_force.c
+++ weapon_force.c
@@ -8,37 +8,29 @@ int main(void)
 {
     int percentage = 95;
     srand(time(NULL));
-    
+
     int weapon =1;
-    char answer[100];
-    int i =0;
-    while(1)
-    {
+    char answer;
+    while(1) {
+        answer = 'a';
         rand() % 100 +1;
-        if(rand() % 100 +1 > percentage)
-        {
+        if(rand() % 100 +1 > percentage) {
             printf("강화에 실패하였습니다. 무기가 파괴됩니다.\n");
             break;
-        }
-        else if(rand() % 100 +1 <=percentage)
-        {
+        } else if(rand() % 100 +1 <=percentage) {
             weapon++;
             printf("강화에 성공하였습니다! 현재 강화등급 +%d \n",weapon);
             percentage -= 5;
         }
         printf("강화를 더 하시겠습니까? 다음 강화확률 %d\% Y/N ",percentage);
-        scanf("%c",&answer[i]);
-        if(answer[i]=='y')
-        {
-            i++;
+        scanf("%c",&answer);
+        if(answer=='y') {
             continue;
-        }
-        else
-        {
+        } else {
             printf("강화를 멈춥니다. 현재 무기 강화는 +%d\n",weapon);
             break;
         }
-        
+
     }
     return 0;
 }
